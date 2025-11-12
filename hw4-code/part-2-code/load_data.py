966bdbb9eef961633570f11360a03d8a02e68a0e
Shengduo Li <sl11793@nyu.edu>
2025-11-12 12:30:31 -0500

    Update load_data.py

--- hw4-code/part-2-code/load_data.py
+++ hw4-code/part-2-code/load_data.py
@@ -125,7 +125,7 @@ def normal_collate_fn(batch):
         sql_tokens = decoder_output_ids[i]
         
         decoder_input = torch.cat([bos_token, sql_tokens[:-1]])
-        decoder_target = sql_tokens[1:] 
+        decoder_target = torch.cat([sql_tokens, torch.tensor([PAD_IDX])])
         decoder_inputs_list.append(decoder_input)
         decoder_targets_list.append(decoder_target)
     
