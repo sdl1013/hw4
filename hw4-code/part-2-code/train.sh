d36455ba1d675e779cb258fd18779d68e1a37ef5
Shengduo Li <sl11793@nyu.edu>
2025-11-12 11:47:15 -0500

    Create train.sh

---
+++ hw4-code/part-2-code/train.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=t5_train
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32GB
+#SBATCH --time=04:00:00
+#SBATCH --output=logs/t5_train_%j.out
+#SBATCH --error=logs/t5_train_%j.err
+#SBATCH --cpus-per-task=4
+
+
+module purge
+conda activate hw4-part-2-nlp
+nvidia-smi
+
+mkdir -p logs checkpoints 
+
+python train_t5.py \
+    --finetune \
+    --learning_rate 1e-4 \
+    --weight_decay 0.01 \
+    --max_n_epochs 10 \
+    --patience_epochs 3 \
+    --num_warmup_epochs 1 \
+    --scheduler_type cosine \
+    --experiment_name baseline
+
+
